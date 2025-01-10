aee99e44add37ed04987c3ecd41e3f0edbb22298
Vincent Daubry <vdaubry@gmail.com>
2025-01-10 22:05:37 +0100

    fix REDIS SSL

--- celery_app.py
+++ celery_app.py
@@ -10,7 +10,14 @@ load_dotenv()
 # Add the `src` directory to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
+# Use the REDIS_URL environment variable, default to localhost for local development
 CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+# Define SSL settings for secure Redis connections
+BROKER_TRANSPORT_OPTIONS = {
+    "ssl_cert_reqs": "CERT_NONE"  # Change to "CERT_REQUIRED" for stricter validation
+}
+
 app = Celery('celery_app', broker=CELERY_BROKER_URL)
 app.conf.update(
     result_backend=CELERY_BROKER_URL,
@@ -23,5 +30,7 @@ app.conf.update(
             'args': ()
         }
     },
+    broker_transport_options=BROKER_TRANSPORT_OPTIONS,
+    result_backend_transport_options=BROKER_TRANSPORT_OPTIONS,  # Ensure backend uses SSL options
     include=['tasks']
 )
