cea04aa53b91ac1f7ad616a10bb8f8d3118504f5
Vincent Daubry <vdaubry@gmail.com>
2025-01-10 19:32:47 +0100

    update crew

--- src/automating_content_creation_for_enhanced_efficiency/crew.py
+++ src/automating_content_creation_for_enhanced_efficiency/crew.py
@@ -1,6 +1,5 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-from crewai_tools import ScrapeWebsiteTool
 from pydantic import BaseModel
 from automating_content_creation_for_enhanced_efficiency.tools.blog_tool import BlogTool
 
@@ -13,26 +12,11 @@ class AutomatingContentCreationForEnhancedEfficiencyCrew():
     """AutomatingContentCreationForEnhancedEfficiency crew"""
 
     @agent
-    def news_monitoring_specialist(self) -> Agent:
+    def dark_humor_philosopher(self) -> Agent:
         return Agent(
-            config=self.agents_config['news_monitoring_specialist'],
+            config=self.agents_config['dark_humor_philosopher'],
             tools=[],
         )
-
-        # @agent
-        # def topic_suggestion_expert(self) -> Agent:
-        #     return Agent(
-        #         config=self.agents_config['topic_suggestion_expert'],
-        #         tools=[],
-        #     )
-        #
-        # @agent
-        # def blog_content_creator(self) -> Agent:
-        #     return Agent(
-        #         config=self.agents_config['blog_content_creator'],
-        #         tools=[],
-        #     )
-        #
     @agent
     def website_publishing_specialist(self) -> Agent:
         return Agent(
@@ -42,27 +26,12 @@ class AutomatingContentCreationForEnhancedEfficiencyCrew():
 
 
     @task
-    def monitor_news_task(self) -> Task:
+    def dark_humor_philosophy_task(self) -> Task:
         return Task(
-            config=self.tasks_config['monitor_news_task'],
-            tools=[ScrapeWebsiteTool()],
+            config=self.tasks_config['dark_humor_philosophy_task'],
+            tools=[],
             output_pydantic=BlogPostOutput
         )
-
-        # @task
-        # def suggest_topic_task(self) -> Task:
-        #     return Task(
-        #         config=self.tasks_config['suggest_topic_task'],
-        #         tools=[],
-        #     )
-        #
-        # @task
-        # def generate_blog_post_task(self) -> Task:
-        #     return Task(
-        #         config=self.tasks_config['generate_blog_post_task'],
-        #         tools=[],
-        #         output_pydantic=BlogPostOutput,
-        #     )
 
     @task
     def publish_blog_post_task(self) -> Task:
